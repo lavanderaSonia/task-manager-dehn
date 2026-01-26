8bfdb3f37bd0bbc1f2bd44a366c0ba92fe0e3a8d
Sonia Lavandera <uo263536@uniovi.es>
2026-01-26 12:57:19 +0100

    feat: Add edit route to router

--- task-manager/src/router/index.ts
+++ task-manager/src/router/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TaskList from '@/views/TaskList.vue'
 import AddTask from '@/views/AddTask.vue'
+import UpdateTask from '@/views/UpdateTask.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/tasks' },
     { path: '/tasks', component: TaskList },
-    { path: '/add-task', component: AddTask }
+    { path: '/add-task', component: AddTask },
+    { path: '/edit/:id', component: UpdateTask }
   ]
 })
